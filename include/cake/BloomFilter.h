9f014d7a846be24447e069f8bc77bd724c1f509b
Mario Rincon Nigro <mario.rincon-nigro@here.com>
2022-02-22 23:13:36 +0100

    .clang-format and corrections in build code

--- include/cake/BloomFilter.h
+++ include/cake/BloomFilter.h
@@ -6,48 +6,43 @@
 #include <cake/Hash.h>
 
 namespace cake {
-    class BloomFilter {
-        public:
-            BloomFilter(size_t size, size_t numHashes) : m_numHashes(numHashes), m_bitArray(size, false) {}
+class BloomFilter {
+public:
+    BloomFilter(size_t size, size_t numHashes)
+        : m_numHashes(numHashes)
+        , m_bitArray(size, false)
+    {
+    }
 
-        template <typename Type>
-        bool contains(const Type& element) const {
-            const auto hash_result = Hash::md5(&element, sizeof(Type));
+    template <typename Type>
+    bool contains(const Type& element) const
+    {
+        const auto hash_result = Hash::md5(&element, sizeof(Type));
 
-            for(size_t i = 0; i < m_numHashes; ++i) {
-                if(!m_bitArray[hash_result[i]])
-                    return false;
-            }
-
-            return true;
+        for (size_t i = 0; i < m_numHashes; ++i) {
+            if (!m_bitArray[hash_result[i]])
+                return false;
         }
 
-        template <typename Type>
-        const void add(const Type& element) {
-            const auto hash_result = Hash::md5(&element, sizeof(Type));
+        return true;
+    }
 
-            for(size_t i = 0; i < m_numHashes; ++i) {
-                m_bitArray[hash_result[i]] = true;
-            }
+    template <typename Type>
+    const void add(const Type& element)
+    {
+        const auto hash_result = Hash::md5(&element, sizeof(Type));
+
+        for (size_t i = 0; i < m_numHashes; ++i) {
+            m_bitArray[hash_result[i]] = true;
         }
+    }
 
-        void clear() {
-            for(size_t i = 0; i < m_bitArray.size(); i++)
-                m_bitArray[i] = false;
-        }
+    void clear();
 
-        double occupancy() const {
-            double occupancy = 0.0;
+    double occupancy() const;
 
-            for(const auto bit: m_bitArray) {
-                occupancy += bit;
-            }
-
-            return occupancy / m_bitArray.size();
-        }
-
-        private:
-            size_t m_size, m_numHashes;
-            std::vector<bool> m_bitArray;
-    };
+private:
+    size_t m_size, m_numHashes;
+    std::vector<bool> m_bitArray;
+};
 }
--- src/Hash.cpp
+++ src/Hash.cpp
@@ -4,7 +4,8 @@
 
 namespace cake {
 namespace Hash {
-    Result md5(const void* data, size_t size) {
+    Result md5(const void* data, size_t size)
+    {
         Result result;
         MD5(reinterpret_cast<const unsigned char*>(data), size, result.data());
         return result;
