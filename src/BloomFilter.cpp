fc28bf5965f053bc752f1b66d8a881b0c09a07df
Mario Rincon Nigro <mario.rincon.nigro@gmail.com>
2022-02-22 23:13:36 +0100

    .clang-format and corrections in build code

---
+++ src/BloomFilter.cpp
@@ -0,0 +1,21 @@
+#include <cake/BloomFilter.h>
+
+namespace cake {
+
+void BloomFilter::clear()
+{
+    for (size_t i = 0; i < m_bitArray.size(); i++)
+        m_bitArray[i] = false;
+}
+
+double BloomFilter::occupancy() const
+{
+    double occupancy = 0.0;
+
+    for (const auto bit : m_bitArray) {
+        occupancy += bit;
+    }
+
+    return occupancy / m_bitArray.size();
+}
+}
