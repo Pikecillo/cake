27c45536195d9fef6e2a73574bf930d41a169ba7
Mario Rincon Nigro <mario.rincon.nigro@gmail.com>
2022-03-10 20:49:19 +0100

    - Further testing of LRU Cache - Improvements to BloomFilter constructor: checking parameter bounds to stay within operational range

--- src/BloomFilter.cpp
+++ src/BloomFilter.cpp
@@ -1,5 +1,6 @@
 #include <cake/BloomFilter.h>
 
+#include <algorithm>
 #include <cmath>
 
 namespace cake {
@@ -11,10 +12,13 @@ const size_t maxNumHashes = 256;
 
 BloomFilter::BloomFilter(size_t expectedCount, double falsePositiveRate = 0.01)
     : m_expectedCount(expectedCount), m_falsePositiveRate(falsePositiveRate) {
+        m_expectedCount = std::max(static_cast<size_t>(1), m_expectedCount);
+        m_falsePositiveRate = std::clamp(m_falsePositiveRate, 0.0005, 0.5);
+
     const double ln_2 = std::log(2.0);
     const double ln2_2 = ln_2 * ln_2;
-    const size_t numBits = -static_cast<long long>(expectedCount) * log(falsePositiveRate) / ln2_2;
-    const size_t numHashes = (numBits / expectedCount) * ln_2;
+    const size_t numBits = -static_cast<long long>(m_expectedCount) * log(falsePositiveRate) / ln2_2;
+    const size_t numHashes = (numBits / m_expectedCount) * ln_2;
 
     m_numHashes = std::min(numHashes, maxNumHashes);
     m_bitArray = std::vector<bool>(std::min(numBits, maxNumBits), false);
--- test/test_lru_cache.cpp
+++ test/test_lru_cache.cpp
@@ -2,18 +2,151 @@
 
 #include <cake/LRUCache.h>
 
+TEST(LRUCacheTest, test_constructor) {
+    {
+    cake::LRUCache<int, std::string> cache(0);
+
+    EXPECT_EQ(1, cache.maxSize());
+    EXPECT_EQ(0, cache.size());
+    }
+
+       {
+    cake::LRUCache<int, std::string> cache(100);
+
+    EXPECT_EQ(100, cache.maxSize());
+    EXPECT_EQ(0, cache.size());
+    }
+}
+
+TEST(LRUCacheTest, test_size_one_cache) {
+    cake::LRUCache<int, std::string> cache(1);
+
+    cache.insert(1, "One");
+    EXPECT_TRUE(cache.contains(1));
+    EXPECT_EQ(1, cache.size());
+
+    cache[2] = "Two";
+    EXPECT_TRUE(cache.contains(2));
+    EXPECT_EQ(1, cache.size());
+
+    cache.remove(2);
+    EXPECT_EQ(0, cache.size());
+
+    cache.insert(3, "Three");
+    EXPECT_TRUE(cache.contains(3));
+    EXPECT_EQ(1, cache.size());
+
+    cache.insert(3, "Drei");
+    EXPECT_TRUE(cache.contains(3));
+    EXPECT_EQ(1, cache.size());
+}
+
 TEST(LRUCacheTest, test_insert) {
     cake::LRUCache<int, std::string> cache(2);
 
     EXPECT_EQ(cache.size(), 0);
+    
     cache.insert(1, "Uno");
     EXPECT_EQ(cache.size(), 1);
+    
     cache.insert(1, "One");
     EXPECT_EQ(cache.size(), 1);
+
     cache.insert(2, "Two");
     EXPECT_EQ(cache.size(), 2);
+
     cache.insert(3, "Three");
     EXPECT_EQ(cache.size(), 2);
+}
+
+TEST(LRUCacheTest, test_remove) {
+    cake::LRUCache<int, std::string> cache(2);
+
+    cache.insert(1, "One");
+    EXPECT_EQ(cache.size(), 1);
+    
+    cache.insert(2, "Two");
+    EXPECT_EQ(cache.size(), 2);
+    
+    EXPECT_TRUE(cache.remove(1));
+    EXPECT_EQ(cache.size(), 1);
+    EXPECT_FALSE(cache.contains(1));
+    EXPECT_TRUE(cache.contains(2));
+    
+    EXPECT_TRUE(cache.remove(2));
+    EXPECT_EQ(cache.size(), 0);
+    EXPECT_FALSE(cache.contains(2));
+
+    EXPECT_FALSE(cache.remove(3));
+    EXPECT_EQ(cache.size(), 0);
+}
+
+TEST(LRUCacheTest, test_access) {
+    cake::LRUCache<int, std::string> cache(2);
+
+    cache.insert(1, "One");
+    EXPECT_EQ("One", cache[1]);
+
+    cache[2];
+    EXPECT_TRUE(cache.contains(2));
+
+    cache[2] = "Two";
+    EXPECT_EQ("Two", cache[2]);
+
+    cache[3] = "Three";
+    EXPECT_TRUE(cache.contains(2));
+}
+
+TEST(LRUCacheTest, test_contains) {
+    cake::LRUCache<int, std::string> cache(2);
+
+    EXPECT_FALSE(cache.contains(1));
+    
+    cache.insert(1, "Uno");
+    EXPECT_TRUE(cache.contains(1));
+    
+    cache.insert(1, "One");
+    EXPECT_TRUE(cache.contains(1));
+    
+    cache.insert(2, "Two");
+    EXPECT_TRUE(cache.contains(2));
+    
+    cache.insert(3, "Three");
+    EXPECT_TRUE(cache.contains(3));
+    EXPECT_FALSE(cache.contains(1));
+    EXPECT_TRUE(cache.contains(2));
+}
+
+TEST(LRUCacheTest, test_lru_eviction_policy) {
+    cake::LRUCache<int, std::string> cache(4);
+
+    cache.insert(1, "One");
+    cache.insert(2, "Two");
+    cache.insert(3, "Three");
+    cache.insert(4, "Four"); // 4 3 2 1
+    cache.insert(5, "Five"); // 5 4 3 2
+    EXPECT_EQ(4, cache.size());
+    EXPECT_FALSE(cache.contains(1));
+
+    EXPECT_TRUE(cache.contains(2)); // 2 5 4 3
+    cache.insert(6, "Six"); // 6 2 5 4
+    EXPECT_EQ(4, cache.size());
+    EXPECT_FALSE(cache.contains(3));
+
+    EXPECT_EQ("Four", cache[4]); // 4 6 2 5
+    cache.insert(7, "Seven"); // 7 4 6 2
+    EXPECT_EQ(4, cache.size());
+    EXPECT_FALSE(cache.contains(5));
+
+    cache[2] = "Zwei"; // 2 7 4 6
+    cache.insert(8, "Eight"); // 8 2 7 4
+    EXPECT_EQ(4, cache.size());
+    EXPECT_FALSE(cache.contains(6));
+
+    EXPECT_TRUE(cache.contains(4));
+    EXPECT_TRUE(cache.contains(7));
+    EXPECT_TRUE(cache.contains(2));
+    EXPECT_TRUE(cache.contains(8));
 }
 
 int main(int argc, char **argv) {
