8994c64da1e9d0433e44575362519a5c4defbfff
Mario Rincon Nigro <mario.rincon.nigro@gmail.com>
2022-03-23 23:09:57 +0100

    Using MurmurHash64A instead of MD5

--- src/Hash.cpp
+++ src/Hash.cpp
@@ -1,13 +1,11 @@
 #include <cake/Hash.h>
 
-#include <openssl/md5.h>
+#include "MurmurHash2.h"
 
 namespace cake {
 namespace Hash {
-Result md5(const void *data, size_t sizeInBytes) {
-    Result result;
-    MD5(reinterpret_cast<const unsigned char *>(data), sizeInBytes, result.data());
-    return result;
+uint64_t murmur64A(const void *data, size_t sizeInBytes, uint64_t seed) {
+    return MurmurHash64A(data, static_cast<int>(sizeInBytes), seed);
 };
 } // namespace Hash
 } // namespace cake
